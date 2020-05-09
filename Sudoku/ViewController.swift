9caed5bb98b6e322d79f742909a226233f7366e4
clarkmyfancy <clark.jack0@gmail.com>
2020-05-09 17:04:13 -0500

    Initial Commit

---
+++ Sudoku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by Jack Clark on 5/9/2020.
+//  Copyright © 2020 Jack Conrad Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
