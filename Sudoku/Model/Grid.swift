44e3baa5eb911ec90708c813d6503b83651138e1
clarkmyfancy <clark.jack0@gmail.com>
2020-05-14 23:03:37 -0500

    initial commit

---
+++ Sudoku/Model/Grid.swift
@@ -0,0 +1,20 @@
+import UIKit
+import Foundation
+
+struct Grid {
+    
+    
+    let subGridBorderWidth: CGFloat = 6.0
+    let cellBorderWidth: CGFloat = 2.0
+    
+//    let puzzleWidth: CGFloat?
+    
+//    init(_ width: CGFloat) {
+//        self.puzzleWidth = width
+//    }
+//    
+    
+//    let puzzleWidth: CGFloat {
+//        return
+//    }
+}
