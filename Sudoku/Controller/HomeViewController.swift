b0d24dc854d857411cf3df26866b2235d8546672
clarkmyfancy <clark.jack0@gmail.com>
2020-05-14 23:05:03 -0500

    initial commit

---
+++ Sudoku/Controller/HomeViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    
+}
+
