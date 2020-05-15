1d0ac9ed15e0dc24cdf8d2efcc23c4c48b8336b5
clarkmyfancy <clark.jack0@gmail.com>
2020-05-14 23:03:06 -0500

    create base interactable cell to build sudoku out of

---
+++ Sudoku/View/SubGridCell.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+
+
+class SubGridCell: UICollectionViewCell {
+
+    @IBOutlet var cellValueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    
+    }
+
+}
+
+//extension SubGridCell: UICollectionViewCellD
