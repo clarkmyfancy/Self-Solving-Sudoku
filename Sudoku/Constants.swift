e8b44277ace46f038ef33f9566e5b38d47cd23d9
clarkmyfancy <clark.jack0@gmail.com>
2020-05-14 23:05:40 -0500

    initial commit with custom cellUI labels

---
+++ Sudoku/Constants.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct Constants {
+    
+    static let subGridReuseIdentifier = "subgrid"
+    static let subGridNibName = "SubGridCell"
+    
+    static let subGrid2ReuseIdentifier = "cell2"
+    static let subGrid2NibName = "SubGridCell2"
+
+    
+}
+
+typealias K = Constants
