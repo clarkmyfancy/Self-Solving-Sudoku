f2c1bf05a3b90321807b22f59eb7f2d1553783c2
clarkmyfancy <clark.jack0@gmail.com>
2020-05-17 17:15:43 -0500

    make readable

--- Sudoku/Controller/HomeViewController.swift
+++ Sudoku/Controller/HomeViewController.swift
@@ -3,19 +3,14 @@ import UIKit
 class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+        super.viewDidLoad()}
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.isNavigationBarHidden = true
-    }
+        navigationController?.isNavigationBarHidden = true}
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        navigationController?.isNavigationBarHidden = false
-    }
-    
-    
+        navigationController?.isNavigationBarHidden = false}
 }
 
