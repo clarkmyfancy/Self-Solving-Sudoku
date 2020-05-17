3b2730c7b1245c0bf8f8c5bc03a056ce82b5fc22
clarkmyfancy <clark.jack0@gmail.com>
2020-05-17 17:11:21 -0500

    initial commit

--- Sudoku/Model/Dimensions.swift
+++ Sudoku/Model/Dimensions.swift
@@ -1,5 +1,12 @@
 import Foundation
+import UIKit
 
 struct Dimensions {
+    let width: CGFloat
+    let height: CGFloat
     
+    init(_ width: CGFloat, _ height: CGFloat) {
+        self.width = width
+        self.height = height
+    }
 }
