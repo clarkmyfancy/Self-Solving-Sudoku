59f5681522056f20c5462d277cbb28bfe5c800c5
Jack Conrad Clark <clark.jack0@gmail.com>
2020-06-05 12:56:33 -0500

    initial commit

--- Sudoku/Model/SudokuManager.swift
+++ Sudoku/Model/SudokuManager.swift
@@ -1,18 +1,74 @@
 import Foundation
+import CoreFoundation
 
 struct SudokuManager {
-    let puzzle: [[Int]] = [
-        [0, 8, 5, 4, 0, 0, 2, 0, 0],
-        [0, 4, 6, 0, 0, 2, 0, 7, 0],
-        [0, 0, 0, 6, 0, 3, 5, 4, 0],
-        
-        [0, 0, 0, 0, 2, 5, 0, 1, 0],
-        [7, 0, 0, 8, 6, 0, 0, 2, 0],
-        [0, 0, 4, 1, 0, 7, 0, 0, 8],
-        
-        [4, 0, 0, 3, 0, 1, 0, 6, 2],
-        [0, 0, 0, 7, 0, 0, 4, 0, 3],
-        [0, 3, 0, 0, 4, 0, 1, 5, 0],
-    ]
+    
+    
+//    var elapsedTime: Float?
+//    var benchTimer: BenchTimer?
+//    var timer = Timer()
+//    
+    let puzzleProvider = PuzzleProvider()
+    var puzzle: [[Int]]?
+    var givens:  [[Bool]]?
+    
+    init() {
+        self.loadPuzzle()
+        self.loadGivens()
+    }
+    
+    mutating func loadPuzzle() {
+        self.puzzle = puzzleProvider.getPuzzle()
+    }
+    
+    mutating func loadGivens() {
+        self.givens = puzzleProvider.getGivens()
+    }
+    
+    func getPuzzle() -> [[Int]]? {
+        if let puzzle = self.puzzle {
+            return puzzle
+        }
+        return nil
+    }
+    
+    
+    
+    
+//    mutating func startTimer() {
+//        self.benchTimer = BenchTimer()
+//        self.timer.invalidate()
+//        self.elapsedTime = 0.0}
+    
+//    mutating func getElapsedTime() -> Float {
+//        timer = Timer.scheduledTimer(withTimeInterval: 0.0, repeats: true, block: { (_) in
+//            self.elapsedTime = Float(self.benchTimer!.elapsed())})
+//        return self.elapsedTime!}
+    
+    func getNumberUsingCoordinates(_ row: Int, _ col: Int) -> Int {
+        if let puzzle = self.puzzle {
+            return puzzle[row][col]
+        }
+        return 0
+    }
+    
+    func determineWhetherNumberWasGivenUsingCoordinates(_ row: Int, _ col: Int) -> Bool {
+        if let givens = self.givens {
+            return givens[row][col] == false
+        }
+        return false
+    }
+    
+    mutating func updateCellAtLocationWithNumber(x row: Int, y col: Int, _ selectedNumber: String) {
+        let newCellValue: Int
+        if selectedNumber == "" {
+            newCellValue = 0
+        } else {
+            newCellValue = Int(selectedNumber)!
+        }
+        puzzle![row][col] = newCellValue
+    }
     
 }
+
+
