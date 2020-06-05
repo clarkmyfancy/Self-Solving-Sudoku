a97e714713cc9d65243f32e84ef3e22e14ce68ce
Jack Conrad Clark <clark.jack0@gmail.com>
2020-06-05 12:57:04 -0500

    initial commit

---
+++ Sudoku/Controller/GameSetupController.swift
@@ -0,0 +1,17 @@
+import Foundation
+import UIKit
+
+class GameSetupController: UIViewController {
+    
+//    var sudokuManager = SudokuManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func onPressedStart(_ sender: UIButton) {
+//        sudokuManager.loadPuzzle()
+    }
+}
+
