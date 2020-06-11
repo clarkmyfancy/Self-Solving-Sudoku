50f5a63205c92c78905a1e7d3f0713d7852bac0d
Jack Conrad Clark <clark.jack0@gmail.com>
2020-06-10 20:21:30 -0500

    add function to get value at a row and column location, refactored constructor

--- Sudoku/Puzzle.swift
+++ Sudoku/Puzzle.swift
@@ -4,24 +4,54 @@ import Foundation
 
 struct Puzzle {
     
-    private var puzzle: [[Int]] = Array(repeating: Array(repeating: 0, count: 9), count: 9)
+    private static var rowCount = 9
+    private static var columnCount = Puzzle.rowCount
+    
+    private var puzzle = [[Int]]()
+    
+    init() {
+        puzzle = Array(repeating: Array(repeating: 0, count: Puzzle.columnCount), count: Puzzle.rowCount)}
+    
+    
+    init(_ puzzle: [[Int]]) {
+        self.puzzle = puzzle}
+    
+    
+    func getRowCount() -> Int {
+        return Puzzle.rowCount}
+    
+    
+    func getColumnCount() -> Int {
+        return Puzzle.columnCount}
+
     
     func row(at i: Int) throws -> [Int] {
         try ifIndexOutOfBoundsThrowError(index: i)
         return puzzle[i]}
+
     
     func col(at j: Int) throws -> [Int] {
         try ifIndexOutOfBoundsThrowError(index: j)
         var column = [Int]()
-        for i in 0..<9 {
+        for i in 0..<Puzzle.rowCount {
             column.append(puzzle[i][j])}
         return column}
     
+    
+    func getCellAt(row i: Int, col j: Int) throws -> Int {
+        try ifIndexOutOfBoundsThrowError(index: i)
+        try ifIndexOutOfBoundsThrowError(index: j)
+        return puzzle[i][j]}
+    
+    
     func ifIndexOutOfBoundsThrowError(index i: Int) throws {
-        if (i < 0) || (i >= 9) {
+        if (i < 0) || (i >= Puzzle.rowCount) {
             throw OutOfBoundsError("Cannot access array, index [\(i)] out of bounds")}}
+    
+    
 }
 
+// think about the scope of this type of error, put it somewhere appropriot
 struct OutOfBoundsError: Error {
     let message: String
     
