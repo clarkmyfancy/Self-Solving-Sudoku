5cea9122d502aa0ef168b77bcb6b6c61a13539fa
Jack Conrad Clark <clark.jack0@gmail.com>
2020-06-10 20:31:01 -0500

    remove all code and start from TDD

--- Sudoku/Services/Puzzle/PuzzleProvider.swift
+++ Sudoku/Services/Puzzle/PuzzleProvider.swift
@@ -1,13 +1,6 @@
-//import Foundation
-//import UIKit
-//
-//struct PuzzleProvider {
-//    
-////    let puzzle: [[Int]]?
-//    
-////    func getPuzzle() -> Puzzle? {
-////        let puzzle = Array(repeating: Array(repeating: 0, count: 9), count: 9) as! Puzzle
-////        return puzzle
-////    }
-//
-//}
+import Foundation
+import UIKit
+
+struct PuzzleProvider {
+    
+}
