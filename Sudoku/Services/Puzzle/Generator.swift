55cfc6ab0bdf796cdb489e2e75b4919fd11a5bb2
Jack Conrad Clark <clark.jack0@gmail.com>
2020-06-26 09:06:28 -0500

    refactor the implementation of #makePuzzle

--- Sudoku/Services/Puzzle/Generator.swift
+++ Sudoku/Services/Puzzle/Generator.swift
@@ -5,12 +5,27 @@ struct Generator {
     func makePuzzle() -> Puzzle {
         // generate [[Int]] and fill with proper values
         // then initialize the puzzle using that [[Int]]
-        return Puzzle()
+//        var puzzleData = [[Int]]()
+//        for _ in 0..<9 {
+//            let row = makeRow()
+//            puzzleData.append(row)
+//        }
+        var candidate = Array(repeating: Array(repeating: 0, count: 9), count: 9)
+        var cellValue = 0
+        for i in 0..<9 {
+            for j in 0..<9 {
+                cellValue += 1
+                candidate[i][j] = cellValue
+            }
+            cellValue = 0
+        }
+        
+        return Puzzle(candidate)
     }
     
-    func makeRow() -> [Int] {
-        return [1,2,3, 4,5,6, 7,8,9]
-    }
+//    func makeRow() -> [Int] {
+//        return [1,2,3, 4,5,6, 7,8,9]
+//    }
     
 }
 
